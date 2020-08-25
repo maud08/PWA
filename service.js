11aa3056528edb337b4a1e0c4b528c27d4398cbc
maud08 <maudinette08@outlook.com>
2020-08-25 13:58:25 +0200

    change color btn

--- service.js
+++ service.js
@@ -2,7 +2,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 
-const version = '0.0.1';
+const version = '0.0.2';
 
 self.addEventListener('install', event => {
     console.log('Install' + version)
