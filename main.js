9687950d905234ef6d3fc06208658708bc22eb68
maud08 <maudinette08@outlook.com>
2020-08-25 13:34:31 +0200

    Button add personnalisé

--- main.js
+++ main.js
@@ -13,3 +13,27 @@ if('serviceWorker' in navigator){
 else{
     console.log('alert', 'Votre navigateur n\'est pas compatible avec notre service')
 }
+
+const installBtn = document.querySelector('.install');
+
+let defferedPrompt;
+
+window.addEventListener('beforeinstallprompt', e => {
+    e.preventDefault()
+    defferedPrompt = e
+    installBtn.style.display = "block";
+})
+
+installBtn.addEventListener('click', e => {
+    installBtn.style.display = "none"
+    defferedPrompt.prompt()
+
+    defferedPrompt.userChoice.then( choiceResult => {
+        if(choiceResult.outcome === 'accepted'){
+            console.log('install ok')
+        }else{
+            console.log('install foireusse')
+        }
+        defferedPrompt = null
+    })
+})
